ebb639405bb65d2ba779052453fbecdd13446617
Pedro Henrique <pedroca0312@gmail.com>
2018-07-13 09:38:47 -0300

    Add files via upload

--- CalculadoraIP/CalculadoraIP.php
+++ CalculadoraIP/CalculadoraIP.php
@@ -1,70 +1,84 @@
 <?php
 
-require_once "index.php";
-
- $oc1= $_POST['1octeto'];
- $oc2= $_POST['2octeto'];
- $oc3= $_POST['3octeto'];
- $oc4= $_POST['4octeto'];
- $final = $_POST['bits'];
-
-$oc4=150;
-$final=25;
+ $oc1= $_POST['oc1'];
+ $oc2= $_POST['oc2'];
+ $oc3= $_POST['oc3'];
+ $oc4= $_POST['oc4'];
+ $final = $_POST['mascara'];
 
  //Endereços em cada sub-rede
 $resultado = 32-$final;
 $enderecos = pow(2,$resultado);
-echo $enderecos." e ";
 
 //Sub-redes
 $sub_redes= 256 / $enderecos;
-echo $sub_redes." e ";
+
 //Hosts, maquinas
 $hosts =$enderecos-2;
-echo $hosts ." e ";
+
 //endereço de rede
 $resto=256-$enderecos;
 while($oc4< $resto){
     $resto = $resto-$enderecos;
 }
 $rede=$resto;
-echo $rede ." e ";
 
 //1 endereço de host
-$host= $rede-1;
+$host= $rede+1;
 
 //Broadcast
 $broadcast= $rede+($enderecos-1);
-echo $broadcast. " e ";
+
 //Ultimo end
 $ult_end=$broadcast -1;
-echo $ult_end ." e ";
+
 //mascara de rede em decimal
 $masc_dec= 256-$enderecos;
-echo $masc_dec ." e ";
 
 if ($oc4 <= 126){
-    echo "classe a";
+    echo "<br>Classe A<br>";
 }
 
-if ($oc4 >= 128 AND $oc4 <= 191){
-    echo "classe b";
+elseif ($oc4 >= 128 AND $oc4 <= 191){
+    echo "<br>Classe B<br>";
 }
 
 
-if ($oc4 >= 192 AND $oc4 <= 223){
-    echo "classe c";
+elseif ($oc4 >= 192 AND $oc4 <= 223){
+    echo "<br>Classe C<br>";
 
-
-}
-
-if ($oc4 >= 224 AND $oc4 <= 239){
-    echo "classe d";
 
 }
 
-if ($oc4 >= 240 AND $oc4 <= 256){
-    echo "classe e";
+elseif ($oc4 >= 224 AND $oc4 <= 239){
+    echo "<br>Classe D<br>";
 
 }
 
+elseif($oc4 >= 240 AND $oc4 <= 256){
+    echo "<br>Classe E<br>";
+
+}else{
+    echo "Não possui classe e é privado.";
+}
+
+if($oc1==10 and $oc2>=0 and $oc2 <=255 and $oc3>=0 and $oc3<=255 and $oc4>=0 and $oc4<=255){
+    echo "Ip privado";
+}
+elseif($oc1==172 and $oc2>=16 and $oc2 <=31 and $oc3>=0 and $oc3<=255 and $oc4>=0 and $oc4<=255){
+    echo "Ip privado";
+}
+elseif($oc1==192 and $oc2==168 and $oc3>=0 and $oc3<=255 and $oc4>=0 and $oc4<=255){
+    echo "Ip privado";
+}else{
+    echo "Ip público";
+}
+
+echo "<ul><li>Quantidade de endereços em cada sub-rede: ".$enderecos."</li>";
+echo "<li>Quantidade de sub redes: ".$sub_redes."</li>";
+echo "<li>Quantidade de hosts/máquinas: ".$hosts."</li>";
+echo "<li>Endereço de rede: ".$rede."</li>";
+echo "<li>Endereço de host: ".$host."</li>";
+echo "<li>Endereço de broadcast: ".$broadcast."</li>";
+echo "<li>Último endereço de host: ".$ult_end."</li>";
+echo "<li>Máscara de rede em decimal: ".$masc_dec."</li></ul>";
--- CalculadoraIP/index.php
+++ CalculadoraIP/index.php
@@ -4,26 +4,22 @@
 
 <script>
     $(document).ready(function(){
-    $('#btcalcular').click(function(){
-        $.get("ip.php",
-            {
-                oc1: $('input[name=oc1]').val(),
-                oc2: $('input[name=oc2]').val(),
-                oc3: $('input[name=oc3]').val(),
-                oc4: $('input[name=oc4]').val(),
-                mascara: $('input[name=bits]').val(),
+        $('#btcalcular').click(function(){
+            $.post("CalculadoraIP.php",
+                {
+                    oc1: $('input[name=oc1]').val(),
+                    oc2: $('input[name=oc2]').val(),
+                    oc3: $('input[name=oc3]').val(),
+                    oc4: $('input[name=oc4]').val(),
+                    mascara: $('input[name=bits]').val()
+                },function (resultado) {
+                    $("#resultados").html(resultado);
+                }
 
-            },
-            function(dados){
-                var txt = "mascara: " $dados,mascara;
-                txt = "<br>Quantidade de endereços: " $dados,totalEnderecos;
-                
-                $("#resultados").html(txt);
-            }
-        )
-    })
+            );
+        });
 
-    })
+    });
 
 </script>
     <meta charset="utf-8">
@@ -31,20 +27,26 @@
 
 
 <body>
+<div id="formulario">
 
-<form method="post">
-    <input type="number" name="oc1">
-    .
-    <input type="number" name="oc2">
-    .
-    <input type="number" name="oc3">
-    .
-    <input type="number" name="oc4">
-    /
-    <input type="number" name="bits">
+        <input type="number" name="oc1">
+        .
+        <input type="number" name="oc2">
+        .
+        <input type="number" name="oc3">
+        .
+        <input type="number" name="oc4">
+        /
+        <input type="number" name="bits">
 
-    <input type="submit" id="btcalcular">Calcular
+        <button id="btcalcular">Calcular</button>
 
-</form>
+</div>
+
+<div id="resultados">
+    
+    
+</div>
+
 </body>
 </html>
